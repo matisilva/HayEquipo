e758e67758a0f6e23610aaf76cc8ed01ddf1d914
Matias <matisilva18@hotmail.com>
2015-08-31 21:12:08 -0300

    Ordenando el proyecto

--- HayEquipo/mysite2/env/lib/python2.7/site-packages/nominatim/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Nominatim API
-http://wiki.openstreetmap.org/wiki/Nominatim"""
-from __future__ import absolute_import
-from .nominatim import Nominatim, NominatimReverse
---
+++ README.md
@@ -0,0 +1,5 @@
+# HayEquipo
+===========
+##Tareas:
+-hacer las apps de Usuario/Comida y Jornada
+-Definir un template y tunearlo static todo.
