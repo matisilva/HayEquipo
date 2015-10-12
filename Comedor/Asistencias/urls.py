50da9f768b8499a2379a80dbccdd7e2c8a10c543
Matias <matisilva18@hotmail.com>
2015-10-12 18:26:52 -0300

    Agrego la idea basica de como hacer formularios, faltan condicionales.

--- Comedor/Asistencias/urls.py
+++ Comedor/Asistencias/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url, include, patterns
 
 from . import views
 
@@ -7,4 +7,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     # ex: /asistencias/5/
     url(r'^(?P<A_id>[0-9]+)/$', views.details, name='details'),
+    # ex: /assist/
+    url(r'^assist/', views.assist, name='assist')
 ]
--- Comedor/Asistencias/views.py
+++ Comedor/Asistencias/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 #For render
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import render_to_response
 #For manage of data
 from models import Asistencia
+#For forms
+from forms import AsistenciaForm
+from django.http import HttpResponseRedirect
+from django.core.context_processors import csrf
 
 
 # Create your views here.
@@ -14,3 +19,17 @@ def index(request):
 
 def details(request, A_id):
     return HttpResponse("Detalles de la asistencia: %s" % A_id)
+
+def assist(request):
+    if request.POST:
+        form = AsistenciaForm(request.POST)
+        #if is_valid():
+        form.save()
+        #Save and reload
+        return HttpResponseRedirect('/asistencias/')
+    else:
+        form = AsistenciaForm()
+    args = {}
+    args.update(csrf(request))
+    args['form'] = form
+    return render_to_response('Asistencias/crear_asistencia.html', args)
